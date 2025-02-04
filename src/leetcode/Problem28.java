09ba0e70034289c412815ac92450fa79894a4609
Muhammaddin Baxridinov <mukhammaddinbakhridinov@gmail.com>
2025-02-04 11:44:57 +0500

    problem28 solved

---
+++ src/leetcode/Problem28.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+public class Problem28 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
